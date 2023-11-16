fcbaf614c72f30541b834f79ad30504852dc9d91
or-m-or <htth815@gmail.com>
2023-11-16 16:48:47 +0900

    로그인 수정,  파일 저장 기능 추가

--- example/crawler.py
+++ example/crawler.py
@@ -1,25 +1,18 @@
+import os
+import json
+import time
+from datetime import datetime
+import random
+import subprocess
+import chromedriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-import chromedriver_autoinstaller
-import pyperclip
-import subprocess
-import time
-import requests
-import random
-import pyautogui
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from urllib.request import Request, urlopen
-import tempfile
-import os
-from datetime import datetime
-import json
+from dotenv import load_dotenv
+
+
+CHROMEDRIVER_PATH = r'C:\Users\thheo\Documents\crawler_test\chromedriver.exe'
 
 
 def random_delay(min_seconds, max_seconds):
@@ -27,121 +20,121 @@ def random_delay(min_seconds, max_seconds):
     time.sleep(random.uniform(min_seconds, max_seconds))
 
 
-# 파일에 내용을 저장하는 함수 (폴더 지정 추가)
-def save_to_file(scrapdata):
+
+def save_to_text_file(scrapdata):
+    """" 크롤링 한 뉴스기사 .txt 파일로 저장 """
+
     folder_name = "results"
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
     current_time = datetime.now().strftime(r"%Y%m%d_%H%M%S")
-    file_title = f"{scrapdata['title'].replace(' ', '_').replace('/', '_')}_{current_time}.txt"
 
-    # scrapdata에서 필요한 값 추출
-    section = scrapdata.get('section', '없음')  # 값이 없으면 '없음'을 기본값으로 사용
-    query = scrapdata.get('query', '없음')
-    title = scrapdata.get('title', '없음')
-    author = scrapdata.get('author', '없음')
-    content = scrapdata.get('content', '없음')
+    # 기사의 제목을 가져와 ':' 또는 ';' 이전의 문자열로 잘라서 파일 이름으로 사용
+    # news_title = scrapdata.get('title', 'No title')
+    # if ':' in news_title:
+    #     file_name_part = news_title.split(':')[0]
+    # elif ';' in news_title:
+    #     file_name_part = news_title.split(';')[0]
+    # else:
+    #     file_name_part = news_title
 
-    # 파일에 작성할 문자열 생성
-    # file_content = f'section: {section}\nquery: {query}\ntitle: {title}\nauthor: {author}\ncontent: \n{content}'
-    file_content = scrapdata['content']
+
+    # file_title = f"{file_name_part}_{current_time}.txt"
+    file_title = f"T_{current_time}.txt"
+
+    news_title = scrapdata.get('title', 'No title')
+    news_author = scrapdata.get('author', 'No author')
+    news_date = scrapdata.get('date', 'No date')
+    news_content = scrapdata.get('content', 'No content')
 
     with open(os.path.join(folder_name, file_title), 'w', encoding='utf-8') as file:
-        # file.write(json.dumps(scrapdata, ensure_ascii=False, indent=4)) 
-        file.write(file_content)
+        file.write(f"title: {news_title}\n")
+        file.write(f"author: {news_author}\n")
+        file.write(f"date: {news_date}\n")
+        file.write(f"content:\n{news_content}\n")
 
 
 
 # nytimes_login, business_crawler 안에서 chrome_driver() 호출하게 변경하기
 def chrome_driver():
-    """ 뉴욕타임스 로그인을 위한 크롬 드라이버 객체를 생성하는 함수 """
+    """ 크롬 드라이버 객체를 생성하는 함수 """
 
-    # # 크롬 브라우저 디버거 모드로 구동 - 삭제 예정
-    # subprocess.Popen(
-    # r'C:\Program Files\Google\Chrome\Application\chrome.exe ' 
-    # r'--remote-debugging-port=9222 '
-    # r'--user-data-dir="C:\chrometemp"'
-    # ) 
+    # # # 크롬 브라우저 디버거 모드로 구동
+    subprocess.Popen(
+    r'C:\Program Files\Google\Chrome\Application\chrome.exe ' 
+    r'--remote-debugging-port=9222 '
+    r'--user-data-dir="C:\chrometemp"'
+    ) 
     
     # Chrome options
-    options = webdriver.ChromeOptions()   # 크롬 옵션 객체 생성
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36" # User-Agent 설정
-    options.add_argument(f'user-agent={user_agent}')
+    options = webdriver.ChromeOptions()
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36" 
+    options.add_argument(f'user-agent={user_agent}')                      # User-Agent 설정
     options.add_argument('headless')                                      # headless 모드로 실행
-    options.add_argument('--incognito')                                   # 크롬 시크릿 모드로 실행 (로그인 기록 삭제..)
+    options.add_argument('--incognito')                                   # 크롬 시크릿 모드로 실행
     options.add_argument('--disable-gpu')                                 # 크롬 시크릿 모드 실행 중 GPU 기반/보조 렌더링을 비활성화
     options.add_argument("--disable-blink-features=AutomationControlled") # 자동화 감지 기능 비활성화
     options.add_argument('--ignore-certificate-errors')                   # SSL 인증 에러 무시
     options.add_argument('--no-sandbox')                                  # 크롬 샌드박스모드 비활성화
-
-    # options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")  # 크롬 디버거 모드로 구동 : 디버거 주소 설정 - 삭제 예정
     
-    # 드라이버 경로 지정 및 크롬 드라이버 객체 생성
-    driverpath = r'C:\Users\thheo\Documents\crawler_test\chromedriver.exe'
-    service = Service(driverpath)
-    driver = webdriver.Chrome(service=service, options=options)
+    options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")  # 크롬 디버거 모드로 구동 : 디버거 주소 설정 - 삭제 예정
 
-    # 드라이버의 모든 쿠키 삭제
+
+    # 드라이버 경로 지정 및 크롬 드라이버 객체 생성
+    # driverpath = os.getenv(CHROMEDRIVER_PATH, chromedriver_autoinstaller.install()) # 드라이버 설치
+    service = Service(CHROMEDRIVER_PATH)
+    driver = webdriver.Chrome(service=service, options=options)
     driver.delete_all_cookies()
+
+    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {"source": """ Object.defineProperty(navigator, 'webdriver', { get: () => undefined }) """})
+    driver.execute_cdp_cmd("Network.enable", {})
+
+
+    # http 표준 헤더 변경
+    headers = {
+        'Accept'      : 'application/json',
+        'Accept-Encoding' : 'gzip, deflate, br',
+        'Accept-Language' : 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Content-Type': 'application/json',
+        'Origin'      : r'https://myaccount.nytimes.com',
+        'Referer'     : r'https://myaccount.nytimes.com/auth/login?response_type=cookie&client_id=vi&redirect_uri=https%3A%2F%2Fwww.nytimes.com%2Fsubscription%2Fonboarding-offer%3FcampaignId%3D7JFJX%26EXIT_URI%3Dhttps%253A%252F%252Fwww.nytimes.com%252F&asset=masthead',
+        'User-Agent'  : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+    }
+    driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": headers})
 
     return driver
     
 
-# 쿠키 때문에 이미 로그인되어 있다면 로그인 안하는 걸로 코드 수정하기
-# 로그인 페이지로 바로 들어가서 로그인 시도 하는 방법으로 수정할 것
 
-
-
-# 로그인이 되었는지 안되었는지 판별하는 코드 수정 필요
 def nytimes_login(driver):
     """ 뉴욕타임스에 로그인을 수행하는 함수 """
+
+    NYTIMES_EMAIL = os.environ['NYTIMES_EMAIL']
+    NYTIMES_PASSWORD = os.environ['NYTIMES_PASSWORD']
     
-    driver.get('https://www.nytimes.com/account')
-    random_delay(3,5)
-    current_url = driver.current_url
-    # exit()
-    if current_url == 'https://www.nytimes.com/account': # 수정
-        print("이미 로그인되어 있습니다.")
-        return True
-    else:
-        try:
-            driver.execute_cdp_cmd("Network.enable", {})
-
-            # 하드코딩 됨 -> 수정 필요
-            headers = {
-                'Accept'      : 'application/json',
-                'Content-Type': 'application/json',
-                'Origin'      : r'https://myaccount.nytimes.com',
-                'Referer'     : r'https://myaccount.nytimes.com/auth/login?response_type=cookie&client_id=vi&redirect_uri=https%3A%2F%2Fwww.nytimes.com%2Fsubscription%2Fonboarding-offer%3FcampaignId%3D7JFJX%26EXIT_URI%3Dhttps%253A%252F%252Fwww.nytimes.com%252F&asset=masthead',
-                'User-Agent'  : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
-                'Req-Details' : '[[it:lui]][[kp:off]]'
-            }
-            driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": headers})
-
-            # 하드코딩 됨 -> 수정 필요
-            nytimes_email = 'dev-admin@rbrain.co.kr'
-            nytimes_password = 'Fpdlsqhdn2023!'
-        
-            # pyperclip.copy(nytimes_email)
-            driver.find_element(By.NAME,'email').send_keys(nytimes_email) # Keys.CONTROL + 'v'
-            random_delay(1, 3)
+    try:
+        driver.get('https://www.nytimes.com/account')
+        random_delay(3,5)  # 로그인 되어 있지 않다면 위 링크로 접속 시 로그인 페이지로 리다이렉트 됨.
+        # exit()
+        if driver.current_url == 'https://www.nytimes.com/account':
+            print("이미 로그인되어 있습니다.")
+            return True
+        else:
+            driver.get(f'https://myaccount.nytimes.com/auth/login?response_type=cookie&client_id=acct&redirect_uri=https%3A%2F%2Fwww.nytimes.com%2Faccount')
+            driver.find_element(By.NAME,'email').send_keys(NYTIMES_EMAIL)
             driver.find_element(By.XPATH, '//*[@id="myAccountAuth"]/div/div/div/form/div/div[4]/button').click() 
             random_delay(1, 3)
-            time.sleep(1)
 
-            # pyperclip.copy(nytimes_password)
-            driver.find_element(By.NAME, 'password').send_keys(nytimes_password) # Keys.CONTROL + 'v'
-            random_delay(1, 3)
+            driver.find_element(By.NAME, 'password').send_keys(NYTIMES_PASSWORD)
             driver.find_element(By.XPATH, '//*[@id="myAccountAuth"]/div/div/form/div/div[2]/button').click()
-            random_delay(3, 7)
-            time.sleep(1)
+            random_delay(2, 3)
             
             print(f"로그인 성공")
             return True
-        except Exception as e:
-            print(f'로그인 중 에러 발생: {e}')
-            return False
+    except Exception as e:
+        print(f'로그인 중 에러 발생: {e}')
+        return False
 
 
 
@@ -184,41 +177,59 @@ def nytimes_getnews(driver, section, query, url):
     scrapData = {}
 
     try:
-        # URL에서 페이지 내용을 가져옴
+        # url 접속하여 html 소스 가져오기
         driver.get(url)
         random_delay(1, 3)
         html = driver.page_source
-        print(html)
         soup = BeautifulSoup(html, 'html.parser')
         
-        # parsedUrl = Request(url, headers={'User-Agent' : "Mozilla/5.0"})
-        # soup = BeautifulSoup(urlopen(parsedUrl), 'html.parser')
-        
+        # 기사 제목 가져오기
+        title = soup.select_one('h1').text.strip()
 
-        # # 기사 제목 가져오기
-        # title = soup.select_one('h1').text.strip()
+        # 기사 내용 가져오기
+        content = ""
+        paragraphs = soup.select('article section p')
 
-        # # 기사 내용 가져오기
-        # content = ""
-        # paragraphs = soup.select('article p')
-        # for paragraph in paragraphs:
-        #     content += paragraph.text + '\n'
+        for paragraph in paragraphs:
+            text = paragraph.text
 
-        # # 기사 작성자 가져오기 (작성자 정보가 있는 경우에만)
-        # author = soup.select_one('article span a')
-        # author = author.text.strip() if author else "작성자 정보 없음"
+            # 텍스트에 'Advertisement' 단어가 포함되어 있으면 그 부분을 제외하고 추가
+            if 'Advertisement' in text:
+                text = text.replace('Advertisement', '')
+            content += text + '\n'
 
-        # # 결과 출력 또는 반환
-        # scrapData = {
-        #     'title': title,
-        #     'author': author,
-        #     'content': content,
-        #     'section': section,
-        #     'query': query
-        # }
-        # return scrapData
+            # 첫 번째 섹션을 만났을 때 플래그 설정 및 더 이상 섹션 크롤링 중지 - 제대로 동작안함.
+            section_encountered = True
+            if 'section' in paragraph.get('class', []):
+                if section_encountered:
+                    section_encountered = False
+                else:
+                    break
 
+        # 기사 작성자 가져오기
+        author = soup.select_one('article span a')
+        author = author.text.strip() if author else "작성자 정보 없음"
 
+        # 기사 작성 일자 가져오기
+        date = soup.select_one('article time')
+        date = date.text.strip() if date else "날짜 정보 없음"
+
+        # 결과 출력 또는 반환
+        scrapData = {
+            'section': section,
+            'query': query, 
+            'title': title,
+            'author': author,
+            'date' : date,
+            'content': content,
+        }
+
+        print("title : \n", title)
+        print("author : \n", author) 
+        print("date : \n",date)
+        print("content : \n", content)
+        return scrapData
+    
     except Exception as e:
         print(f'nytimes_getnews()에서 에러 발생: {e}')
         return {}
@@ -227,25 +238,21 @@ def nytimes_getnews(driver, section, query, url):
 
 
 if __name__=="__main__":
-    driver = chrome_driver() 
-    login_status = nytimes_login(driver) # True or False
+    driver = chrome_driver()
+    login_status = nytimes_login(driver) # 로그인 상태 반환
 
-    # 로그인이 성공했다면 크롤링 수행
+    # 로그인에 성공했다면 크롤링 수행
     if login_status:      
         scrapList = []
         section = 'business'
-        query = 'r200'
+        query = 'hamas'
         crawling_num = 3
 
         news_url_list = nytimes_newslist(driver, section, query, crawling_num)
         for news_url in news_url_list:
             scrapdata = nytimes_getnews(driver, section, query, news_url)
             scrapList.append(scrapdata)
-            save_to_file(scrapdata)
-            
-
-
-        print("scrapList : \n", scrapList)
+            save_to_text_file(scrapdata)
     else:
         print("로그인 실패로 인해 크롤링을 수행하지 않습니다.")
 
