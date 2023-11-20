7c2b3e1d9378c2c987523c661a9b25f55a162ffd
or-m-or <htth815@gmail.com>
2023-11-20 19:00:31 +0900

    llm test

--- summary_generator.py
+++ summary_generator.py
@@ -1,31 +1,51 @@
-import pytest
-
-from langchain.chat_models import ChatOpenAI
-from llama_index import ServiceContext, LLMPredictor, Document
-from llama_index.node_parser import SimpleNodeParser
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+import os
+import openai
+import logging
+import sys
+import nest_asyncio
 from llama_index import (
     SimpleDirectoryReader,
-    LLMPredictor,
     ServiceContext,
-    Document,
     get_response_synthesizer,
-    ListIndex
 )
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.indices.document_summary import DocumentSummaryIndex
+from llama_index.llms import OpenAI
 
 
-documents = SimpleDirectoryReader(r"C:\Users\thheo\Documents\crawler_test\documents\crawling_results").load_data()
-index = VectorStoreIndex.from_documents(documents)
 
-query_engine = index.as_query_engine()
-response = query_engine.query("Summarize in one sentence")
-print(response)
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+nest_asyncio.apply() 
+# 비동기 IO 작업을 중첩하여 사용할 수 있게 함. 
+# (동일한 스레드 내에서 여러 이벤트 루프 중첩해여 사용)
+
+# load data
+news_docs = []
+docs = SimpleDirectoryReader(
+    input_files=[r"sample\business_hamas_1_1117_150040.txt"]
+).load_data()
+news_docs.extend(docs)
 
 
-def initialize_index():
-    
+# LLM
+chatgpt = OpenAI(temperature=0, model="gpt-3.5-turbo")
+service_context = ServiceContext.from_defaults(llm=chatgpt, chunk_size=1024)
+
+# Index default mode
+response_synthesizer = get_response_synthesizer(
+    response_mode="tree_summarize", use_async=True
+)
+doc_summary_index = DocumentSummaryIndex.from_documents(
+    news_docs,
+    service_context=service_context,
+    response_synthesizer=response_synthesizer,
+    show_progress=True,
+)
+
+
+
 
 
